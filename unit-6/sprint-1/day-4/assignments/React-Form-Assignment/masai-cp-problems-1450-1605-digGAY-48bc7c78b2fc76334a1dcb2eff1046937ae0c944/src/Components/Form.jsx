e5e9d72adeb186fbb9778333d7dcae11b9d591c1
abuzar-shaikh <110722376+abuzar-shaikh@users.noreply.github.com>
2023-06-24 21:10:09 +0530

    React-Form-Assignment

---
+++ unit-6/sprint-1/day-4/assignments/React-Form-Assignment/masai-cp-problems-1450-1605-digGAY-48bc7c78b2fc76334a1dcb2eff1046937ae0c944/src/Components/Form.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import "./Form.css";
+
+const Form = () => {
+  return (
+    <div>
+      <div>
+        <form onSubmit={() => {}}>
+          {/* First Name */}
+
+          {/* Last Name */}
+
+          {/* Email */}
+
+          {/* Password */}
+
+          {/* Phone number */}
+
+          {/* Country Select Tage */}
+
+          {/* Birth Date Selector */}
+
+          {/* Profile Pic Selector */}
+
+          {/* Married Status Selector */}
+
+          {/* Gender Selector */}
+
+          {/* Submit Button - input type submit */}
+        </form>
+        {/* if form submitted then show ShowFormData component here */}
+      </div>
+    </div>
+  );
+};
+
+export default Form;
