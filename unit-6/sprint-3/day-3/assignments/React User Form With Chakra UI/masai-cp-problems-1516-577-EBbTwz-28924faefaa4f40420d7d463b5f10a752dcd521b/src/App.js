4f2a52e549e7f7dc823e52edae08833e86869245
abuzar-shaikh <110722376+abuzar-shaikh@users.noreply.github.com>
2023-07-13 23:32:05 +0530

    React User form with Chakra Ui

--- unit-6/sprint-3/day-3/assignments/React User Form With Chakra UI/masai-cp-problems-1516-577-EBbTwz-28924faefaa4f40420d7d463b5f10a752dcd521b/src/App.js
+++ unit-6/sprint-3/day-3/assignments/React User Form With Chakra UI/masai-cp-problems-1516-577-EBbTwz-28924faefaa4f40420d7d463b5f10a752dcd521b/src/App.js
@@ -1,5 +1,23 @@
-import { useState } from "react";
+import { useState, useReducer } from "react";
 import "./App.css";
+import {
+  Box,
+  Button,
+  Center,
+  Checkbox,
+  FormControl,
+  Heading,
+  FormLabel,
+  Input,
+  Select,
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+} from "@chakra-ui/react";
+import { type } from "@testing-library/user-event/dist/type";
+import { UserRow } from "./Component/UserRow";
 
 const initialState = {
   name: "",
@@ -9,50 +27,97 @@ const initialState = {
 };
 
 //should have the cases "name", "gender", "role", "maritalStatus", and "reset" along with the default cases
-const reducer = (state, action) => {};
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "name": {
+      return { ...state, name: action.payload };
+    }
+    case "gender": {
+      return { ...state, gender: action.payload };
+    }
+    case "role": {
+      return { ...state, role: action.payload };
+    }
+    case "maritalStatus": {
+      return { ...state, maritalStatus: action.payload };
+    }
+    case "reset": {
+      return initialState;
+    }
+  }
+  return state;
+};
 
 function App() {
   // note : Remove below const and instead import them from chakra import the following components from
-  const Box = () => <div />;
-  const Button = () => <div />;
-  const Center = () => <div />;
-  const Checkbox = () => <div />;
-  const FormControl = () => <div />;
-  const Heading = () => <div />;
-  const FormLabel = () => <div />;
-  const Input = () => <div />;
-  const Select = () => <div />;
+
   // use Table Component from chakra ui to display the details
   const TableContainer = () => <div />;
 
   // import and use the useReducer hook here, with the reducer function and the initialState.
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   //store the data in this object variable when you click on the submit button, to render, the data in the UI.
   const [submittedData, setSubmittedData] = useState([]);
-
+  const HandalSubmit = (e) => {
+    e.preventDefault();
+    setSubmittedData([...submittedData, state]);
+    dispatch({ type: "reset" });
+  };
+  console.log(submittedData);
   return (
     <div className="App">
       <Heading as="h1">Login Form</Heading>
       <Box>
-        <form data-testid="form-element">
+        <form data-testid="form-element" onSubmit={HandalSubmit}>
           <FormControl>
-            <FormLabel></FormLabel>
+            <FormLabel>Name </FormLabel>
 
-            <Input type="text" name="name" />
-            <FormLabel></FormLabel>
-            <Select name="gender" data-testid="gender-select">
-              <option></option>
-              <option></option>
-              <option></option>
+            <Input
+              type="text"
+              name="name"
+              placeholder="Name"
+              value={state.name}
+              onChange={(e) =>
+                dispatch({ type: "name", payload: e.target.value })
+              }
+            />
+            <FormLabel>Gender </FormLabel>
+            <Select
+              name="gender"
+              data-testid="gender-select"
+              value={state.gender}
+              onChange={(e) =>
+                dispatch({ type: "gender", payload: e.target.value })
+              }
+            >
+              <option>Male</option>
+              <option>Female</option>
+              <option>Prefer Not to Say</option>
             </Select>
-            <FormLabel></FormLabel>
-            <Select name="role" data-testid="role-select">
-              <option></option>
-              <option></option>
-              <option></option>
+            <FormLabel>Role </FormLabel>
+            <Select
+              name="role"
+              data-testid="role-select"
+              value={state.role}
+              onChange={(e) =>
+                dispatch({ type: "role", payload: e.target.value })
+              }
+            >
+              <option>FrontEnd Developer</option>
+              <option>BackEnd Developer</option>
+              <option>FullStack Developer</option>
             </Select>
 
-            <Checkbox name="maritalStatus">Married</Checkbox>
+            <Checkbox
+              name="maritalStatus"
+              checked={state.maritalStatus}
+              onChange={(e) =>
+                dispatch({ type: "maritalStatus", payload: e.target.checked })
+              }
+            >
+              Married
+            </Checkbox>
             <Center>
               <Button variant="solid" type="submit">
                 SUBMIT
@@ -62,8 +127,34 @@ function App() {
         </form>
       </Box>
       {/* map through the submittedData and render UserRow component to display the data in tabular format ELSE print "no users found"  in there is no user data */}
-      <Heading data-testid="no-user-container"></Heading>
-
+      {submittedData.length > 0 ? (
+        <Table variant="striped" colorScheme="teal">
+          <Thead>
+            <Tr>
+              <Th>s.NO</Th>
+              <Th>USER</Th>
+              <Th>GENDER</Th>
+              <Th>ROLE</Th>
+              <Th>MARITAL STATUS</Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            {submittedData.map((elem, index) => {
+              return (
+                <UserRow
+                  name={elem.name}
+                  gender={elem.gender}
+                  role={elem.role}
+                  maritalStatus={elem.maritalStatus}
+                  id={index + 1}
+                />
+              );
+            })}
+          </Tbody>
+        </Table>
+      ) : (
+        <Heading data-testid="no-user-container">no users found</Heading>
+      )}
     </div>
   );
 }
--- unit-6/sprint-3/day-3/assignments/React User Form With Chakra UI/masai-cp-problems-1516-577-EBbTwz-28924faefaa4f40420d7d463b5f10a752dcd521b/src/Component/UserRow.jsx
+++ unit-6/sprint-3/day-3/assignments/React User Form With Chakra UI/masai-cp-problems-1516-577-EBbTwz-28924faefaa4f40420d7d463b5f10a752dcd521b/src/Component/UserRow.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { Tr, Td } from "@chakra-ui/react";
 
 // use Td component from  from chakra ui to display the user details
 const UserRow = ({ name, gender, role, maritalStatus, id }) => {
-  return <></>;
+  console.log(maritalStatus);
+  return (
+    <Tr>
+      <Td>{id}</Td>
+      <Td>{name}</Td>
+      <Td>{gender}</Td>
+      <Td>{role}</Td>
+      {maritalStatus ? <Td>married</Td> : <Td>unmarried</Td>}
+    </Tr>
+  );
 };
 export { UserRow };
