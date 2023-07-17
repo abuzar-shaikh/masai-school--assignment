e5667c7460bec5fc3eb5766765a0db0d376cdd62
abuzar-shaikh <110722376+abuzar-shaikh@users.noreply.github.com>
2023-07-18 00:16:16 +0530

    React Movie Search App

--- unit-6/sprint-3/day-2/assignments/React Movie Search App/masai-cp-problems-1515-1267-DJAEst-de25ca4ddc43123d69b7746e93f358f8840d0fe8/src/Components/Dashboard.jsx
+++ unit-6/sprint-3/day-2/assignments/React Movie Search App/masai-cp-problems-1515-1267-DJAEst-de25ca4ddc43123d69b7746e93f358f8840d0fe8/src/Components/Dashboard.jsx
@@ -1,19 +1,43 @@
-import React from 'react'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 export default function Dashboard() {
-  
+  const [data, setdata] = useState([]);
+  const [query, setquery] = useState();
+  useEffect(() => {
+   const debounce=setTimeout(()=>{
+    if(query){
+      axios
+      .get(
+        `http://localhost:8080/movies?q=${query}`
+      )
+      .then((res) => setdata(res.data));
+    }
+   
+   },1000);
+   return()=>clearTimeout(debounce)
+  }, [query]);
+ 
+console.log(data)
   return (
-    <div>
-       <input
-          style = {{padding:"5px", margin: "10px", width: 200}}
-          type = "text"
-          data-testid = "search_key"
-          placeholder = "Search Movie Username"
+ <div>
+      <input
+        style={{ padding: "5px", margin: "10px", width: 200 }}
+        type="text"
+        data-testid="search_key"
+        placeholder="Search Movie Username"
+        onChange={(e) => setquery(e.target.value)}
       />
-      <div data-testid = "movie_results" >
+      <div data-testid="movie_results">
         {/* Add movie results based of search key here */}
+      {data.map((elem,index)=>{
+        return(<div key={index}>
+          <h1>Title:{elem.title}</h1>
+          <h1>Year:{elem.year}</h1>
+        </div>
+        )
+      })}
       </div>
-      
-    </div>
-  )
+      </div>
+  );
 }
